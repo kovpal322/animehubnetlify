274a53c906d003144692d474598e482d45c63e07
kovpal322 <145595168+kovpal322@users.noreply.github.com>
2024-04-19 12:03:33 +0200

    asdasd

--- Animehub/src/App.jsx
+++ Animehub/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { w3cwebsocket } from 'websocket';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
